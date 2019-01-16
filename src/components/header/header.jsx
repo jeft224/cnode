baaff282783ac8506cc2b11707efa6ae615d7e89
Abert <1248824453@qq.com>
2019-01-16 16:51:16 +0800

    添加资源

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,15 +3,13 @@ import React, { Component } from 'react';
 class CHeader extends Component {
   
   render(){
-    return (
-      <header>
+    return <header>
         <div className="header__logo">
-          <i className="header__logo--menu" />
-          <i className="header__logo--back" />
+          <i className="header__logo--menu iconfont icon-menu" />
+          <i className="header__logo--back iconfont icon-fanhui" />
           <img src="https://o4j806krb.qnssl.com/public/images/cnodejs_light.svg" alt="" />
         </div>
-      </header>
-    )
+      </header>;
   }
 
 }
