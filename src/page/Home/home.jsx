86932d6c1ec22aef3454e9567ddf422f0ae81a15
Abert <1248824453@qq.com>
2019-01-20 17:43:06 +0800

    添加侧边栏涉及的模块

--- src/page/Home/home.jsx
+++ src/page/Home/home.jsx
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import CHeader from '../../components/header/header';
-import News from '../news/news';
+import News from '../News/news';
 import AsideMenu from '../../components/menu/menu';
 // import './home.scss';
 class Home extends Component{
   state = {
     isMenuShow: false,
   }
+  setMenuShowStatus = () => {
+    this.setState({
+      isMenuShow:!this.state.isMenuShow
+    })
+  }
   render(){
     return [
-      <CHeader />,
-      <AsideMenu/>,
+      <CHeader setMenuShowStatus={this.setMenuShowStatus}/>,
+      <AsideMenu isMenuShow ={this.state.isMenuShow} setMenuShowStatus={this.setMenuShowStatus}/>,
       <News />
     ]
   }
---
+++ src/page/Login/login.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  render () {
+    return (
+      <div className="login">
+        <div className="login-header">
+          <i></i>登录
+        </div>
+        登录页面
+      </div>
+    )
+  }
+}
+
+export default Login;
