fa3aaa4222258bbdf0c531b5a176b3afe89cde14
Abert <1248824453@qq.com>
2019-02-17 10:13:39 +0800

    基本完成登录页

--- src/page/Login/login.jsx
+++ src/page/Login/login.jsx
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
-
+import './login.scss';
 class Login extends Component {
   render () {
     return (
       <div className="login">
-        <div className="login-header">
-          <i></i>登录
+        <div className="login_header">
+          <i />
+          <h2>登录</h2>
+          {/* <button>你好</button> */}
         </div>
-        登录页面
+        <div className="login_form">
+          <input type="text" placeholder="请输入Access Token" />
+          <button className="btn_check">验证</button>
+        </div>
+
+        <div className="intro">
+          <ul>
+            <li>为了更好地体验请先登录☺</li>
+            <li>请输入CNode账号的Access Token</li>
+            <li>在个人中心可以获取Access Token</li>
+          </ul>
+        </div>
       </div>
-    )
+    );
   }
 }
 
